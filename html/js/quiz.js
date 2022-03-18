9629182452151d6662980c3661e62139864fbf59
Jack011601@gmail.com <adduser@DESKTOP-214IHU5.localdomain>
2022-03-18 10:32:16 -0500

    finished code for quiz, still not working

--- html/js/quiz.js
+++ html/js/quiz.js
@@ -1,73 +1,104 @@
 
-const quizContainer = document.getElementById('quiz');
+(function(){
 
-const resultsContainer = document.getElementById('results');
-
-const submitButton = document.getElementById('submit');
-
-function buildQuiz(){
-
-    const output = [];
+    // put the rest of your code here
     
-    myQuestions.forEach( 
-        (currentQuestion, questionNumber) => {
+    function buildQuiz(){
 
-            const answers = [];
-
-            for(letter in currentQuestion.answers){
-
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value = "${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
+        const output = [];
+        
+        myQuestions.forEach( 
+            (currentQuestion, questionNumber) => {
+    
+                const answers = [];
+    
+                for(letter in currentQuestion.answers){
+    
+                    answers.push(
+                        `<label>
+                        <input type="radio" name="question${questionNumber}" value = "${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                        </label>`
+                    );
+                }
+    
+                output.push(
+                    `<div class="question"> ${currentQuestion.question} </div>
+                     <div class="answers"> ${answers.join('')} </div>`
                 );
             }
-
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                 <div class="answers"> ${answers.join('')} </div>`
-            );
-        }
-    );
-        quizContainer.innerHTML = output.join('');
-}
-
-function showResults(){}
-
-buildQuiz();
-
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-    {
-        question: "What is Luna's nickname?",
-        answers: {
-            a: "Tuba",
-            b: "Tuna",
-            c: "Zumba"
+        );
+            quizContainer.innerHTML = output.join('');
+    }
+    
+    function showResults(){
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+    
+        let numCorrect = 0;
+    
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+    
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=questions${questionsNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    
+            if(userAnswer === currentQuestion.correctAnswer){
+                numCorrect++;
+    
+                answerContainers[questionNumber].style.color = 'lightgreen';
+    
+            }
+    
+            else{
+                
+                answerContainer[questionNumber].style.color = 'red';
+            }
+       });   
+    
+       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    
+    }
+    
+    const quizContainer = document.getElementById('quiz');
+    
+    const resultsContainer = document.getElementById('results');
+    
+    const submitButton = document.getElementById('submit');
+    
+    const myQuestions = [
+        {
+            question: "What is Luna's nickname?",
+            answers: {
+                a: "Tuba",
+                b: "Tuna",
+                c: "Zumba"
+            },
+            correctAnswer: "b"
         },
-        correctAnswer: "b"
-    },
-    {
-        question: "How did I come up with Milo's name?",
-        answers: {
-            a: "A song that I heard",
-            b: "My uncle's dog had the same name",
-            c: "I used a random name generator"
+        {
+            question: "How did I come up with Milo's name?",
+            answers: {
+                a: "A song that I heard",
+                b: "My uncle's dog had the same name",
+                c: "I used a random name generator"
+            },
+            correctAnswer: "a"
         },
-        correctAnswer: "a"
-    },
-    {
-        questions: "What was Kora's foster name (before I renamed her)?",
-        answers: {
-            a: "Missy",
-            b: "Snickers",
-            c: "Tasha"
+        {
+            questions: "What was Kora's foster name (before I renamed her)?",
+            answers: {
+                a: "Missy",
+                b: "Snickers",
+                c: "Tasha"
+            },
+            correctAnswer: "c"
         },
-        correctAnswer: "c"
-    },
-];
+    ];
+    
+    buildQuiz();
+    
+    submitButton.addEventListener('click', showResults);
 
-
+  })();
+  
